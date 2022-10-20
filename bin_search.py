c5ecbf455bb9c2ae8a104f3530ee26f82bc49f1d
Jonathan Yoo <yoosoybeans@yoosoybeans-5.local>
2022-10-19 20:54:33 -0400

    added the legend

--- bin_search.py
+++ bin_search.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.patches as mpatches
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -21,9 +22,14 @@ colors[end] = 'red'
 colors[mid] = 'orange'
 
 def update(frame):
+
+    global colors
     global start
     global end
     global mid
+
+    print(start, end)
+    plt.bar(data, data, color = colors, edgecolor = colors)
 
     colors[start] = 'green'
     colors[end] = 'green'
@@ -31,7 +37,7 @@ def update(frame):
 
     if target == data[mid]:
         colors[mid] = 'blue'
-        ani.event_source.stop()
+        # ani.event_source.stop()
     else:
         if target < data[mid]:
             end = mid
@@ -43,12 +49,16 @@ def update(frame):
         colors[end] = 'red'
         colors[mid] = 'orange'
 
-    plt.bar(data, data, color = colors)
+fig, ax = plt.subplots()
+red_patch = mpatches.Patch(color='red', label='Start and End Indexes')
+orange_patch = mpatches.Patch(color='orange', label='Middle Value')
+blue_patch = mpatches.Patch(color='blue', label='Search Target Found')
+ax.legend(handles=[red_patch, orange_patch, blue_patch])
+ax.set_title(f"Target:{target}")
 
-fig = plt.figure()
+plt.bar(data, data, color = colors, edgecolor = colors)
 
-plt.bar(data, data, color = colors)
+ani = FuncAnimation(fig, update, frames=7, interval=1000)
+ani.save("bin_search.gif", writer='Pillow', fps=60)
 
-ani = FuncAnimation(fig, update, frames=len(colors), interval=1000)
-
-plt.show()
+# plt.show()
